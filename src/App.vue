a45d1df994e46327ac73fe2e24c08e097154a5a0
Alga <akgleal@gmail.com>
2019-02-11 15:12:59 +1100

    initial project

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <!--<div id="app">-->
+  <!--<div id="nav">-->
+  <!--<router-link to="/">Home</router-link> |-->
+  <!--<router-link to="/about">About</router-link>-->
+  <!--</div>-->
+  <!--<router-view />-->
+  <!--</div>-->
+
+  <v-app id="app" dark>
+    <!--<v-navigation-drawer-->
+    <!--v-model="drawer"-->
+    <!--app-->
+    <!--&gt;-->
+    <!--<v-list dense>-->
+    <!--<v-list-tile @click="">-->
+    <!--<v-list-tile-action>-->
+    <!--<v-icon>dashboard</v-icon>-->
+    <!--</v-list-tile-action>-->
+    <!--<v-list-tile-content>-->
+    <!--<v-list-tile-title>Dashboard</v-list-tile-title>-->
+    <!--</v-list-tile-content>-->
+    <!--</v-list-tile>-->
+    <!--<v-list-tile @click="">-->
+    <!--<v-list-tile-action>-->
+    <!--<v-icon>settings</v-icon>-->
+    <!--</v-list-tile-action>-->
+    <!--<v-list-tile-content>-->
+    <!--<v-list-tile-title>Settings</v-list-tile-title>-->
+    <!--</v-list-tile-content>-->
+    <!--</v-list-tile>-->
+    <!--</v-list>-->
+    <!--</v-navigation-drawer>-->
+    <v-toolbar app fixed clipped-left>
+      <!--<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>-->
+      <v-toolbar-title>Cluedo Notepad</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            <router-view />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span
+        >{{ new Date().getFullYear() }} -
+        <a href="www.alga.me" target="_blank">Alga.me</a></span
+      >
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    // source: String
+  }
+};
+</script>
+
+<style>
+@import "../node_modules/vuetify/dist/vuetify.min.css";
+</style>
---
+++ src/router.js
@@ -0,0 +1,41 @@
+import Vue from "vue";
+import Router from "vue-router";
+import store from "./store";
+import Notepad from "./views/Notepad.vue";
+import ConfigModule from "./views/ConfigModule.vue";
+
+Vue.use(Router);
+
+const router = new Router({
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Notepad
+    },
+    {
+      path: "/config",
+      name: "config",
+      component: ConfigModule
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    }
+  ]
+});
+
+router.beforeEach((from, to, next) => {
+  if (!store.getters.isPlayersSet && from.name !== "config") {
+    return next({ name: "config" });
+  }
+
+  next();
+});
+
+export default router;
